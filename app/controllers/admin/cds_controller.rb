8ccf46f83102ae807b4d03c03d1de32d7792ee55
美坂朱里 <shuri1201@icloud.com>
2019-12-16 06:37:59 +0000

    テスト

--- app/controllers/admin/cds_controller.rb
+++ app/controllers/admin/cds_controller.rb
@@ -1,25 +1,48 @@
 class Admin::CdsController < Admin::ApplicationController
   def index
-    @cd = Cd.all
+    @cds = Cd.all
   end
 
   def new
     @cd = Cd.new
     @disc = @cd.discs.build
     @song = @disc.musics.build
+
+
+    @labels = Label.all
+    @artists = Artist.all
+    @genres = Genre.all
+
   end
 
   def create
-   @cd = Cd.new(cd_params)
-   @cd.save
-   redirect_to admin_cds_path(@cd.id)
+    @cd = Cd.new(cd_params)
+    @cd.save
+    redirect_to admin_cds_path(@cd.id)
   end
 
   def edit
+     @artists =Artist.all
+     @cd = Cd.find(params[:id])
   end
 
   def update
+     @cd.update(cd_params)
+     redirect_to public_user_path(@cd.id)
   end
+
+  def searchs
+    @search_option = params[:option]
+    @search_text = params[:search]
+    if @search_option == "2"
+      @search_a = Artist.search(params[:search], @search_option)
+    elsif @sesarch_option == "3"
+      @search_t = Cd.search(params[:search], @search_option)
+    else
+      @search_a = Artist.search(params[:search], @search_option)
+      @search_t = Cd.search(params[:search], @search_option)
+  end
+end
 
 private
 
