0455288268814a40362843a6774e0c35e73eddbc
美坂朱里 <shuri1201@icloud.com>
2019-12-18 10:48:53 +0000

    commit

--- app/controllers/admin/restocks_controller.rb
+++ app/controllers/admin/restocks_controller.rb
@@ -1,6 +1,8 @@
 class Admin::RestocksController < Admin::ApplicationController
 
   def index
+    @restocks = Restock.all
+    @artists = Artist.all
   end
 
   def new
@@ -23,7 +25,7 @@ class Admin::RestocksController < Admin::ApplicationController
 private
 
     def restock_params
-     params.require(:restock).permit()
+     params.require(:restock).permit(:restock_date, :restock_count)
     end
 
 
