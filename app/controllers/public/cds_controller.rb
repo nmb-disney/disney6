f746cb5a45e9baab9b6f776e6e69f0917baa3d96
ryo2626 <r26.mth@gmail.com>
2019-12-21 04:39:06 +0000

    ryo

--- app/controllers/public/cds_controller.rb
+++ app/controllers/public/cds_controller.rb
@@ -21,13 +21,11 @@ class Public::CdsController < Public::ApplicationController
     @search_text = params[:search]
     if @search_option == "2"
       @search_a = Artist.search(params[:search], @search_option)
-    elsif @sesarch_option == "3"
+    elsif @search_option == "3"
       @search_t = Cd.search(params[:search], @search_option)
-
     else
     	@search_a = Artist.search(params[:search], @search_option)
     	@search_t = Cd.search(params[:search], @search_option)
-
     end
 
   end
