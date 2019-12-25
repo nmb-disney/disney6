2091dea668ade65a49ee6e166ff0e1b86d0128ac
美坂朱里 <shuri1201@icloud.com>
2019-12-25 06:09:08 +0000

    w

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,21 @@
 class Admin::OrdersController < Admin::ApplicationController
 
   def index
-  	@orders = Order.page(params[:page]).per(10)
+    @orders = Order.all
+    @orders = @orders.order("deliver_status")
+    @order = Order.page(params[:page]).per(10)
   end
 
   def show
+    @order = Order.find(params[:id])
+    @tax = 1.1
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.deliver_status = 2
+    @order.save
+    redirect_to admin_orders_path
   end
 
 
@@ -12,5 +23,9 @@ private
 
     def order_params
      params.require(:order).permit(:id)
+    end
+
+    def order_status_deliver_params
+      params.require(:deliver_status).permit(1,2)
     end
 end
