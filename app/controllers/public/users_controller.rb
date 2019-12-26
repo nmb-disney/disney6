7dfa9022469e205980f568e6c64ad0be362110a4
ryo2626 <r26.mth@gmail.com>
2019-12-26 09:47:58 +0000

    ryo

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,7 +2,6 @@ class Public::UsersController < Public::ApplicationController
   before_action :authenticate_user!#これでログインしていない人がURLを指定しなかった場合ログインのページに強制的に飛ばす
   before_action :user_find, only: [:edit, :update, :destroy, :out]
   before_action :current_user, only: [:show, :update , :destroy]
-
   def show
     @users = User.page(params[:page]).per(10)
     @user = current_user
