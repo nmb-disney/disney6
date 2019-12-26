8d96a841fab61f9e1b42b07c30482bcd4a219ec8
Your Name <you@example.com>
2019-12-26 11:34:13 +0000

    a

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,7 +2,7 @@ class Public::UsersController < Public::ApplicationController
   before_action :authenticate_user!#これでログインしていない人がURLを指定しなかった場合ログインのページに強制的に飛ばす
   before_action :user_find, only: [:edit, :update, :destroy, :out]
   before_action :current_user, only: [:show, :edit, :update , :destroy]
-   
+
   def show
     @users = User.page(params[:page]).per(10)
     @user = current_user
@@ -44,11 +44,11 @@ class Public::UsersController < Public::ApplicationController
     @user = User.find(params[:id])
   end
 
-  def current_user
-    @user = User.find(params[:id])
-   if current_user != user
-    redirect_to user_path(current_user.id)
-   end
-  end
+  # def current_user
+  #   @user = User.find(params[:id])
+  #  if current_user != user
+  #   redirect_to user_path(current_user.id)
+  #  end
+  # end
 
 end
